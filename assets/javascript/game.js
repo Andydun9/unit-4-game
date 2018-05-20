4f2687b612573a302449475ba94deeff4e85933c
Your Name <you@example.com>
2018-05-20 15:23:40 -0700

    updated with intial JavaScript

---
+++ assets/javascript/game.js
@@ -0,0 +1,17 @@
+
+
+//var randomNumberDisplay=document.getElementById("getNumber");
+//link.onload=getNumber
+
+function getNumber(){
+
+    var randomNumber=Math.floor((Math.random()* 100)+1);
+    window.onload(randomNumber)
+    console.log(getNumber)
+}
+//$(document).ready(function(){
+
+
+
+
+//})
